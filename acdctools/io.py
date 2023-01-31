abc201ef4d2906550b58c486b2c42caeb17c0cea
Francesco <padovaf@tcd.ie>
2023-01-31 14:38:06 +0100

    get segm.npz filepath

--- acdctools/io.py
+++ acdctools/io.py
@@ -6,7 +6,7 @@ def get_filepath_from_channel_name(images_path, channel_name):
     h5_aligned_path = ''
     h5_path = ''
     npz_aligned_path = ''
-    tif_path = ''
+    img_path = ''
     for file in path.listdir(images_path):
         channelDataPath = os.path.join(images_path, file)
         if file.endswith(f'{channel_name}_aligned.h5'):
@@ -15,8 +15,8 @@ def get_filepath_from_channel_name(images_path, channel_name):
             h5_path = channelDataPath
         elif file.endswith(f'{channel_name}_aligned.npz'):
             npz_aligned_path = channelDataPath
-        elif file.endswith(f'{channel_name}.tif'):
-            tif_path = channelDataPath
+        elif file.endswith(f'{channel_name}.tif') or file.endswith(f'{channel_name}.npz'):
+            img_path = channelDataPath
     
     if h5_aligned_path:
         return h5_aligned_path
@@ -24,7 +24,7 @@ def get_filepath_from_channel_name(images_path, channel_name):
         return h5_path
     elif npz_aligned_path:
         return npz_aligned_path
-    elif tif_path:
-        return tif_path
+    elif img_path:
+        return img_path
     else:
         return ''
