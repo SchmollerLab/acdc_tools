3f9ae8b8e6ef7721a41e880e207e41b581c82e03
Francesco <padovaf@tcd.ie>
2023-02-16 09:11:15 +0100

    allow append text in path.newfilepath

--- acdctools/path.py
+++ acdctools/path.py
@@ -9,16 +9,30 @@ def listdir(path):
         and not f == 'desktop.ini'
     ])
 
-def newfilepath(file_path):
+def newfilepath(file_path, appended_text: str=None):
     if not os.path.exists(file_path):
         return file_path
-
+    
     folder_path = os.path.dirname(file_path)
     filename = os.path.basename(file_path)
     filename, ext = os.path.splitext(filename)
+
+    if appended_text is not None:
+        if appended_text.startswith('_'):
+            appended_text = appended_text.lstrip('_')
+
+    if appended_text is not None:
+        new_filename = f'{filename}_{appended_text}{ext}'
+        new_filepath = os.path.join(folder_path, new_filename)
+        if not os.path.exists(new_filepath):
+            return new_filepath
+    
     i = 0
     while True:
-        new_filename = f'{filename}_{i+1}{ext}'
+        if appended_text is not None:
+            new_filename = f'{filename}_{appended_text}_{i+1}{ext}'
+        else:
+            new_filename = f'{filename}_{i+1}{ext}'
         new_filepath = os.path.join(folder_path, new_filename)
         if not os.path.exists(new_filepath):
             return new_filepath
